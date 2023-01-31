91e5c61566e25c24fb371c92d4b3c95c25a86416
panvla <pandvlado@gmail.com>
2023-01-31 23:03:18 +0100

    added some front end function.

---
+++ employeemanagerB/src/app/app.component.ts
@@ -0,0 +1,65 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+
+  public employees: Employee[];
+  public deleteEmployee: Employee;
+  public editEmployee: Employee;
+  
+  constructor(private employeeService: EmployeeService){}
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[])=>{
+        this.employees = response;
+        console.log(this.employees);
+      },
+      (error: HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    );
+  }
+
+  public onAddEmployee(addForm: NgForm): void {
+    document.getElementById('add-employee-form').click();
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      
+    );
+  }
+
+  public onOpenModal(employee: Employee, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if(mode === 'add'){
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if(mode === 'edit'){
+      this.editEmployee = employee;
+      button.setAttribute('data-target', '#editEmployeeModal');
+    }
+    if(mode === 'delete'){
+      this.deleteEmployee = employee;
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container.appendChild(button);
+    button.click();
+  }
+
+
+}
